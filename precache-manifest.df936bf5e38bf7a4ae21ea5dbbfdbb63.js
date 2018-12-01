c8e6ffad9877d721abf230206f6a3589339cb6f7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-01 15:46:01 +0000

    Deploy STEP-tw/gol-vprince001 to github.com/STEP-tw/gol-vprince001.git:gh-pages

---
+++ precache-manifest.df936bf5e38bf7a4ae21ea5dbbfdbb63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "111bc2381620a2a91f3c",
+    "url": "/gol-vprince001/static/js/runtime~main.111bc238.js"
+  },
+  {
+    "revision": "008440f2c68de491c496",
+    "url": "/gol-vprince001/static/js/main.008440f2.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/gol-vprince001/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "008440f2c68de491c496",
+    "url": "/gol-vprince001/static/css/main.270fbfc3.chunk.css"
+  },
+  {
+    "revision": "67c6cf256a836869b515c154d2a9578d",
+    "url": "/gol-vprince001/index.html"
+  }
+];
